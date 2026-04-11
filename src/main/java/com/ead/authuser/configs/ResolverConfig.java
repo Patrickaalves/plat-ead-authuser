abdadbbe43fcb12f9800e0380b583bb3201a625a
Patrick <patrickalvessc@gmail.com>
2026-04-11 20:49:51 -0300

    Adicionando Cors no projeto

--- src/main/java/com/ead/authuser/configs/ResolverConfig.java
+++ src/main/java/com/ead/authuser/configs/ResolverConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,12 @@ public class ResolverConfig implements WebMvcConfigurer {
         pageableResolver.setFallbackPageable(PageRequest.of(0, 10));
 
         argumentResolvers.add(pageableResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/users/**").allowedOrigins("*");
+//        registry.addMapping("/auth/**").allowedOrigins("*");
+        registry.addMapping("/**");
     }
 }
